ea091ab19e2315f6d8dd160ea271f8055b1ccded
Vanessa Isidro <vnisidro@traxiontech.net>
2023-08-16 15:38:35 +0800

    Setting up database

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,3 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {}
+
+/*
+  Options To Create A Database
+  1) Install Postgres on local machine  =>  localhost:5432
+  2) Host Postgres in cloud
+*/
+
