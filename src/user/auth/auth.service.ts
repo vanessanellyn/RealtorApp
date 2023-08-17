1ff7da3237c4b6cc93ff113aed37a8484970a7a6
Vanessa Isidro <vnisidro@traxiontech.net>
2023-08-17 12:01:25 +0800

    Hashing password and generating user data

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from "bcryptjs";
+import * as jwt from 'jsonwebtoken';
+import { UserType } from '@prisma/client';
 
 interface SignupParams {
   email: string;
@@ -12,7 +15,7 @@ interface SignupParams {
 export class AuthService {
   constructor(private readonly prismaService: PrismaService){}
 
-  async signup({email}: SignupParams) {
+  async signup({email, password, name, phone}: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: {
         email
@@ -20,5 +23,28 @@ export class AuthService {
     });
     
     if(userExists) throw new ConflictException("Email is already in use.");
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Pass the user data to db
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER
+      },
+    });
+
+    // Generating token for hashing
+    const token = await jwt.sign({
+      name,
+      id: user.id
+    }, process.env.JSON_TOKEN_KEY, {
+      expiresIn: 3600000
+    });
+
+    return token;
   }
 }
